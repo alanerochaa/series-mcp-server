dbdbbd11a22cd0af9ae283d7081a6bfdcba19569
alanerochaa <alanerochaa@gmail.com>
2026-05-21 19:15:10 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/alane/tvmaze_mcp/TvmazeMcpServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.alane.tvmaze_mcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvmazeMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvmazeMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alane/tvmaze_mcp/TvmazeMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alane.tvmaze_mcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TvmazeMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
